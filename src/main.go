408758a361c6ab6f2f88006e80ec06d92b832e03
Erick Flores <xerickcua@gmail.com>
2024-02-20 00:05:50 -0500

    update 5

--- src/main.go
+++ src/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/aldinokemal/go-whatsapp-web-multidevice/cmd"
+import (
+	"fmt"
+	"net/http"
+	"time"
+	"github.com/aldinokemal/go-whatsapp-web-multidevice/cmd"
+)
+
+func reloadConnection() {
+	go func() {
+		for range time.Tick(5 * time.Minute) {
+			resp, err := http.Get("https://whatsappvet3-tmohe8jg.b4a.run")
+			if err != nil {
+				fmt.Println("Hubo un error al ejecutar la API:", err)
+				continue
+			}
+			defer resp.Body.Close()
+			fmt.Println("La API se ejecutó con éxito")
+		}
+	}()
+}
 
 func main() {
+	go reloadConnection()
 	cmd.Execute()
 }
+
